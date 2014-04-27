140299159d4cfdf47c7d928f446feaeacdf8ccde
Michael Kirk <michael.winter.kirk@gmail.com>
2014-04-27 01:09:42 -0400

    Add nil tests

--- Objective-C-KoansTests/AboutNil.m
+++ Objective-C-KoansTests/AboutNil.m
@@ -8,11 +8,28 @@
 
 #import <XCTest/XCTest.h>
 
+
+static NSString * const __ = @"FILL ME IN";
+
 @interface AboutNil : XCTestCase
 
 @end
 
 @implementation AboutNil
 
+- (void)testYouDontGetNullPointerExceptionsWhenCallingMethodsOnNil
+{
+    id object = __;
+    
+    XCTAssertNoThrow([object stringValue]);
+}
+
+- (void)testNilAndNULLHaveTheSameValue
+{
+    //Nil and NULL evaluate to the same value, but NULL
+    //should only be used in C code, whereas nil should
+    //be used in Objective-c code
+    XCTAssertEqualObjects(__, NULL);
+}
 
 @end
