896071bc4fee4ad5f9664aa6c02d76c63ec06fed
Michael Kirk <michael.winter.kirk@gmail.com>
2014-04-27 00:56:45 -0400

    Add object tests

---
+++ Objective-C-KoansTests/AboutObjects.m
@@ -0,0 +1,68 @@
+//
+//  AboutObjects.m
+//  Objective-C-Koans
+//
+//  Created by Michael Kirk on 4/27/14.
+//  Copyright (c) 2014 Michael Kirk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+static id const ___ = nil;
+static NSString * const __ = @"FILL ME IN";
+
+@interface AboutObjects : XCTestCase
+
+@end
+
+@implementation AboutObjects
+
+- (void)testObjectDeclaration
+{
+    //Objective-C objects must be declared as seen below with the
+    //'*' to denote that you are declaring a pointer. Typically, you
+    //will only use the '*' when declaring the variable.
+    NSString *string = @"string";
+    
+    XCTAssertNotNil(___);
+    
+    //The exception is when declaring a variable with 'id' since it
+    //implies the pointer.
+    id anotherString = @"anotherString";
+    
+    XCTAssertNotNil(___);
+}
+
+- (void)testAssertObjects
+{
+    NSString *objectOne = @"Object";
+    NSString *objectTwo = objectOne;
+    
+    //XCTAssertEqualObjects calls 'isEqual:' using the two passed objects
+    //and should be used when comparing Objective-c objects in tests
+    XCTAssertEqualObjects(__, objectTwo);
+}
+
+- (void)testObjectPointerEquality
+{
+    UILabel *labelOne = [[UILabel alloc] init];
+    UILabel *labelTwo = labelOne;
+    
+    //Using '==' with Objective-C objects will always compare their memory address.
+    //In other words, only use '==' when you want to know if two objects are
+    //literally the exact same object.
+    XCTAssertTrue(___ == labelTwo);
+}
+
+- (void)testObjectIntrospection
+{
+    UILabel *label = [[UILabel alloc] init];
+    
+    //Tests if the receiver is an instance of the class
+    XCTAssertTrue([___ isMemberOfClass:[UILabel class]]);
+    
+    //Tests if the receiver is an instance or descendant of the class
+    XCTAssertTrue([___ isKindOfClass:[NSObject class]]);
+}
+
+@end
