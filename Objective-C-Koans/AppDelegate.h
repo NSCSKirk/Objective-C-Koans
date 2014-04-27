22b7dcd59b4f5a4994f95353c079caed05e0db2b
Michael Kirk <michael.winter.kirk@gmail.com>
2014-04-26 23:22:17 -0400

    Initial Commit

---
+++ Objective-C-Koans/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Objective-C-Koans
+//
+//  Created by Michael Kirk on 4/26/14.
+//  Copyright (c) 2014 Michael Kirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
