11bd8acbedbba850685d65de93380ca6c06f25b9
Michael Kirk <michael.winter.kirk@gmail.com>
2014-05-01 21:07:44 -0400

    Add blocks tests

--- Objective-C-KoansTests/AboutBlocks.m
+++ Objective-C-KoansTests/AboutBlocks.m
@@ -1,4 +1,4 @@
-\//
+//
 //  AboutBlocks.m
 //  Objective-C-Koans
 //
@@ -9,26 +9,101 @@
 #import <XCTest/XCTest.h>
 
 @interface AboutBlocks : XCTestCase
-
+@property (nonatomic, copy) NSString *result;
 @end
 
 @implementation AboutBlocks
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+    self.result = nil;
 }
 
-- (void)testExample
+- (void)testBlocksAreStoredPiecesOfCode
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    void (^block)(BOOL) = ^(BOOL value){
+        XCTAssertEqual(YES, value);
+    };
+    
+    block(YES);
+}
+
+- (void)testBlocksCaptureTheirLexicalScope
+{
+    NSString *string = @"A String";
+    
+    void (^block)(void) = ^{
+        XCTAssertEqualObjects(@"A String", string);
+    };
+    
+    block();
+}
+
+- (void)testSettingAVariableFromOutsideTheBlockRequiresTheBlockPrefix
+{
+    __block int number = 0;
+    
+    void (^block)(void) = ^{
+        number = 5;
+        XCTAssertEqual(5, number);
+    };
+    
+    block();
+}
+
+- (void)testBlocksCanReturnValues
+{
+    BOOL (^block)(void) = ^{
+        return YES;
+    };
+    
+    BOOL result = block();
+    
+    XCTAssertEqual(YES, result);
+}
+
+- (void)testBlockCanBePassedAsParameters
+{
+    [self methodAcceptingBlock:^(NSString *string) {
+        XCTAssertEqualObjects(@"Block String", string);
+    }];
+}
+
+- (void)methodAcceptingBlock:(void(^)(NSString *string))block
+{
+    //Checking for the block's existance is very important. If
+    //this check is omitted, and nil or null is passed for the block,
+    //the application will crash due to EXEC_BAD_ACCESS.
+    if (block)
+    {
+        block(@"Block String");
+    }
+}
+
+- (void)testIfSelfIsUsedInsideABlockSelfWillBeRetainedWithIt
+{
+    [self methodAcceptingBlock:^(NSString *string) {
+        //Blocks capture their lexical scope so they can be stored
+        //for later execution. Sincle variables could be deallocated
+        //between the time the block is stored and executed, the block
+        //retains any variables accessed outside of itself. The same
+        //applies to 'self'. If the block is not released and it
+        //retains a reference to a class, a strong reference cycle
+        //is created, preventing class deallocation.
+        self.result = @"String Result";
+    }];
+    
+    XCTAssertEqualObjects(@"String Result", self.result);
+}
+
+- (void)testAvoidStringReferenceCyclesWithBlocksAndSelfByUSingWeakReferences
+{
+    __weak AboutBlocks *weakSelf = self;
+    [self methodAcceptingBlock:^(NSString *string) {
+        weakSelf.result = @"Weak String Result";
+    }];
+    
+    XCTAssertEqualObjects(@"Weak String Result", self.result);
 }
 
 @end
