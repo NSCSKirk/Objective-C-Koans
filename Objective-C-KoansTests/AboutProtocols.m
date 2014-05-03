9796ec3aa218fe12ed15ea30207f5fad447f0e56
Michael Kirk <michael.winter.kirk@gmail.com>
2014-05-02 20:48:07 -0400

    Add empty protocol test case

---
+++ Objective-C-KoansTests/AboutProtocols.m
@@ -0,0 +1,19 @@
+//
+//  AboutProtocols.m
+//  Objective-C-Koans
+//
+//  Created by Michael Kirk on 5/2/14.
+//  Copyright (c) 2014 Michael Kirk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AboutProtocols : XCTestCase
+
+@end
+
+@implementation AboutProtocols
+
+
+
+@end
