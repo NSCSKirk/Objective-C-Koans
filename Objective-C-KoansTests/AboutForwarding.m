ed081735a0fbd5d597566d710bcae0b52f1bac1a
Michael Kirk <michael.winter.kirk@gmail.com>
2014-05-03 14:42:57 -0400

    Add empty forwarding test case

---
+++ Objective-C-KoansTests/AboutForwarding.m
@@ -0,0 +1,19 @@
+//
+//  AboutForwarding.m
+//  Objective-C-Koans
+//
+//  Created by Michael Kirk on 5/3/14.
+//  Copyright (c) 2014 Michael Kirk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AboutForwarding : XCTestCase
+
+@end
+
+@implementation AboutForwarding
+
+
+
+@end
