1014df72a44591c510ca10693def502338163a86
Michael Kirk <michael.winter.kirk@gmail.com>
2014-05-04 19:21:11 -0400

    Add empty category test case

---
+++ Objective-C-KoansTests/AboutCategories.m
@@ -0,0 +1,19 @@
+//
+//  AboutCategories.m
+//  Objective-C-Koans
+//
+//  Created by Michael Kirk on 5/4/14.
+//  Copyright (c) 2014 Michael Kirk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AboutCategories : XCTestCase
+
+@end
+
+@implementation AboutCategories
+
+
+
+@end
