ca5cf19fae1f55f57a9bb9e23692ef5ec12ccc10
Michael Kirk <michael.winter.kirk@gmail.com>
2014-05-05 23:48:44 -0400

    Add a few URL tests

--- Objective-C-KoansTests/AboutURLs.m
+++ Objective-C-KoansTests/AboutURLs.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "KoansDefines.h"
 
 @interface AboutURLs : XCTestCase
 
@@ -14,6 +15,33 @@
 
 @implementation AboutURLs
 
+- (void)testNSURLRepresentsAUniformResourceLocator
+{
+    NSURL *url = [NSURL URLWithString:@"http://github.com"];
+    
+    XCTAssertEqualObjects(@"http://github.com", url.absoluteString);
+}
 
+- (void)testURLsCanBeCreatedInPiecesInTheInitializer
+{
+    NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:@"github.com" path:@"/WinterKirk"];
+    
+    XCTAssertEqualObjects(@"http://github.com/WinterKirk", url.absoluteString);
+}
+
+- (void)testURLComponentsCanBeExtractedFromAnNSURL
+{
+    NSURL *url = [NSURL URLWithString:@"http://Mike:Password1@github.com:8080/Example/repo.git?method=ssh&branch=master#default"];
+    
+    XCTAssertEqualObjects(__, [url scheme]);
+    XCTAssertEqualObjects(__, [url user]);
+    XCTAssertEqualObjects(__, [url password]);
+    XCTAssertEqualObjects(__, [url host]);
+    XCTAssertEqualObjects(__, [url port]);
+    XCTAssertEqualObjects(__, [url path]);
+    XCTAssertEqualObjects(__, [url pathExtension]);
+    XCTAssertEqualObjects(__, [url query]);
+    XCTAssertEqualObjects(__, [url fragment]);
+}
 
 @end
